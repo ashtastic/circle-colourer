f8c2dfe8d19975f8e67c59f5d7f90bf559922681
ashtastic <amurr20@gmail.com>
2016-03-15 16:37:52 -0400

    Initial setup.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var $body = $('html');
+
+
+
+
+
+
+
+
+//var $body = $('html');
+
+//$body.on('keydown', function () {
+
+  //var $ball = $('<div>');
+
+  //$ball.addClass('ball');
+
+  //$body.append($ball);
+
+  //$ball.css('left', Math.random() * 1000);
+
+  //$ball.css('top', Math.random() * 1000);
+
+  //$ball.css('background-color', Math.random());
+//});
